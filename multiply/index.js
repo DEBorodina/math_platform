3b1ed6426759f486630afad6555efe7430801b68
Daria Borodina <dasha.borodina2003@mail.ru>
2021-12-26 15:51:06 +0300

    js

---
+++ multiply/index.js
@@ -0,0 +1,8 @@
+let task = document.getElementById("task");
+let answer = document.getElementById("answer");
+let checkButton = document.getElementById("check");
+let backButton = document.getElementById("back");
+
+function generatePair(){
+    
+}
