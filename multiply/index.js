7f9b556d8901e1fabdf0580a36ec56c0cfcde556
Daria Borodina <dasha.borodina2003@mail.ru>
2021-12-26 16:18:45 +0300

    random

--- multiply/index.js
+++ multiply/index.js
@@ -3,6 +3,10 @@ let answer = document.getElementById("answer");
 let checkButton = document.getElementById("check");
 let backButton = document.getElementById("back");
 
-function generatePair(){
-    
+function generateNum(){
+    let firstNum = Math.floor(Math.random()*100)/100 + Math.floor(Math.random()*100);
+    let secondNum = Math.floor(Math.random()*100)/100 + Math.floor(Math.random()*100);
+    task.innerHTML = firstNum + " &#183; " + secondNum;
 }
+
+generateNum();
