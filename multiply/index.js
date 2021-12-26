75ae46f334ebd78f8472cf19c998654fddc27bba
Daria Borodina <dasha.borodina2003@mail.ru>
2021-12-26 17:34:36 +0300

    multiply

--- multiply/index.js
+++ multiply/index.js
@@ -1,12 +1,72 @@
 let task = document.getElementById("task");
 let answer = document.getElementById("answer");
 let checkButton = document.getElementById("check");
-let backButton = document.getElementById("back");
+let wrong = document.getElementById("wrong");
+let score = document.getElementById("score");
+let rightAnswer;
 
-function generateNum(){
-    let firstNum = Math.floor(Math.random()*100)/100 + Math.floor(Math.random()*100);
-    let secondNum = Math.floor(Math.random()*100)/100 + Math.floor(Math.random()*100);
-    task.innerHTML = firstNum + " &#183; " + secondNum;
+function setLocalStorage(){
+    if(localStorage.getItem('multiplyScore')===null){
+        localStorage.setItem('multiplyScore', JSON.stringify(1));
+        return 1;
+    }else{
+        let item = JSON.parse(localStorage.getItem('multiplyScore'));
+        localStorage.setItem('multiplyScore', JSON.stringify(++item));
+        return item;
+    }
 }
 
-generateNum();
+function setLastNums(mass){
+    if(localStorage.getItem('multiplyLastNums')===null){
+        localStorage.setItem('multiplyLastNums', JSON.stringify(mass));
+    }else{
+        localStorage.setItem('multiplyLastNums', JSON.stringify(mass));
+    }
+}
+
+function generateNum(){
+    let res, firstNum, secondNum;
+
+    while(true){
+        firstNum = Math.floor(Math.random()*100)/100 + Math.floor(Math.random()*10);
+        secondNum = Math.floor(Math.random()*100)/100 + Math.floor(Math.random()*10);
+        res = firstNum + " &#183; " + secondNum;
+        if(res.length<=20){
+            break;
+        }
+    }
+
+    setLastNums([firstNum,secondNum]);
+
+    task.innerHTML=res;
+    return (firstNum*secondNum).toFixed(4);
+}
+
+
+
+function App(){
+    if(localStorage.getItem('multiplyLastNums')!==null){
+        let mass = JSON.parse(localStorage.getItem('multiplyLastNums'));
+        task.innerHTML = mass[0] + " &#183; " + mass[1];
+        rightAnswer = (mass[0]*mass[1]).toFixed(4);
+    }else{
+        rightAnswer = generateNum();
+    }    
+    score.innerHTML = `Твой счет: ${(localStorage.getItem('multiplyScore')===null)?0:localStorage.getItem('multiplyScore')}`;
+    checkButton.addEventListener('click',()=>{
+        if(Math.abs(answer.value-rightAnswer)<=0.00001){
+            rightAnswer = generateNum();
+            answer.value="";
+            setLocalStorage();
+            score.innerHTML = `Твой счет: ${localStorage.getItem('multiplyScore')}`;
+        }else{
+            wrong.innerHTML="Попробуй еще раз!";
+        }
+    })
+}
+
+
+
+App();
+
+
